a689ee84c209c07e7dad7d1549680bdba07759b1
Dmitry <schedamian@gmail.com>
2024-11-19 13:44:14 +0300

    add receive

---
+++ js/sliders.js
@@ -0,0 +1,59 @@
+const masterSliderContainer = document.querySelector('.master__slider-1');
+
+console.log(masterSliderContainer);
+if (masterSliderContainer) {
+  const masterSSlider = new Swiper('.master__slider-1', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    navigation: {
+      prevEl: '.master-prev-1',
+      nextEl: '.master-next-1',
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 16,
+      },
+      992: {
+        spaceBetween: 40,
+      },
+    },
+  });
+}
+const masterSliderContainer2 = document.querySelector('.master__slider-2');
+
+console.log(masterSliderContainer2);
+if (masterSliderContainer2) {
+  const masterSSlider2 = new Swiper('.master__slider-2', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
+    navigation: {
+      prevEl: '.master-prev-2',
+      nextEl: '.master-next-2',
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 16,
+      },
+      992: {
+        spaceBetween: 40,
+      },
+    },
+  });
+}
+
+const receiveSliderContainer = document.querySelector('.master__slider-2');
+
+if (receiveSliderContainer) {
+  const receiveSSlider = new Swiper('.receive__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    breakpoints: {
+      320: {
+        spaceBetween: 16,
+      },
+      992: {
+        spaceBetween: 0,
+      },
+    },
+  });
+}
